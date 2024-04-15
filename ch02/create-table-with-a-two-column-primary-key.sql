83a627cbaee658bf290cfc63b89257be2c2adbf3
David Stevenson <drstevenson1958@yahoo.com>
2024-04-15 10:15:19 -0400

    Create table with check named check constraints

--- ch02/create-table-with-a-two-column-primary-key.sql
+++ ch02/create-table-with-a-two-column-primary-key.sql
@@ -1,9 +1,13 @@
 use land ;
 
+drop table if exists high_temperature ;
+
 create table high_temperature
 (
     city				varchar(50),
     year				int,
     high_temperature	int,
+    constraint high_temperature_year_check check ( year between 1880 and 2200),
+    constraint high_temperature_check check ( high_temperature < 200 ),
     primary key (city, year)
 );
