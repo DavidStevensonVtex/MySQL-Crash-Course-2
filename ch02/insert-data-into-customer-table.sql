16999b530f768d50d337ac416ecf465f8305ec67
David Stevenson <drstevenson1958@yahoo.com>
2024-04-15 09:38:04 -0400

    insert data into customer table

---
+++ ch02/insert-data-into-customer-table.sql
@@ -0,0 +1,10 @@
+use feedback ;
+
+-- If you want to load the customer table with data, run these commands:
+insert into customer (customer_id, first_name, last_name, address)
+values
+(1, 'Bob', 'Smith', '12 Dreary Lane'),
+(2, 'Sally', 'Jones', '76 Boulevard Meugler'),
+(3, 'Karen', 'Bellyacher', '354 Main Street');
+
+select * from customer ;
