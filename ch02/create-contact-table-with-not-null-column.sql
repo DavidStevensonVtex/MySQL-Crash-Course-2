62cada27cf7378b914ca0a349493e8ce7546fad3
David Stevenson <drstevenson1958@yahoo.com>
2024-04-15 10:09:43 -0400

    Create contact table with not null column

---
+++ ch02/create-contact-table-with-not-null-column.sql
@@ -0,0 +1,11 @@
+use feedback ;
+
+create table contact
+(
+    contact_id     int,
+    name           varchar(50)  not null,
+    city           varchar(50),
+    phone          varchar(20),
+    email_address  varchar(50)  unique,
+    primary key(contact_id)
+);
