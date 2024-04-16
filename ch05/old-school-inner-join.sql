593977acf027ed1f8decd9e02ddad439b2030076
David Stevenson <drstevenson1958@yahoo.com>
2024-04-16 12:31:42 -0400

    Old school inner join

---
+++ ch05/old-school-inner-join.sql
@@ -0,0 +1,9 @@
+use subway ;
+
+-- Old School Inner Join
+select  s.subway_system,
+        s.city,
+        c.country
+from    subway_system as s,
+        country as c
+where   s.country_code = c.country_code;
