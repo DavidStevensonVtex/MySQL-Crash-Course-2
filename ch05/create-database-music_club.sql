47b2c2b2a1ff3b5c5a95e209e87f6bef516493d7
David Stevenson <drstevenson1958@yahoo.com>
2024-04-16 12:25:02 -0400

    Create database music_club (to demonstrate self joins)

---
+++ ch05/create-database-music_club.sql
@@ -0,0 +1,23 @@
+drop database if exists music_club ;
+
+-- Create table for the self join example below
+create database music_club;
+
+use music_club;
+
+create table music_preference
+	(
+	music_fan		varchar(100),
+	favorite_genre	varchar(100)
+	);
+	
+insert into music_preference (music_fan, favorite_genre)
+values
+('Bob',		'Reggae'),
+('Earl',	'Bluegrass'),
+('Ella',	'Jazz'),
+('Peter',	'Reggae'),
+('Benny',	'Jazz'),
+('Bunny',	'Reggae'),
+('Sierra',	'Bluegrass'),
+('Billie',	'Jazz');
