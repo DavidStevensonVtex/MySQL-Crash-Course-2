4b039c52b8f92ee31f1ec45eba7f5c5bf2e4c45a
David Stevenson <drstevenson1958@yahoo.com>
2024-04-16 12:22:06 -0400

    Create database restaurant - to demonstrate cross joins

---
+++ ch05/create-database-restaurant.sql
@@ -0,0 +1,28 @@
+drop database if exists restaurant ;
+
+-- Building the tables for the the cross join example below
+create database restaurant;
+
+use restaurant;
+
+create table main_dish
+	(
+	main_item  varchar(100)
+	);
+	
+create table side_dish
+	(
+	side_item  varchar(100)
+	);
+
+insert into main_dish (main_item)
+values
+('steak'),
+('chicken'),
+('ham');
+
+insert into side_dish (side_item)
+values
+('french fries'),
+('rice'),
+('potato chips');
