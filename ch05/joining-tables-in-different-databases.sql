7bc9bb578e73ff2e48953ddb0d0a755f49dbeab2
David Stevenson <drstevenson1958@yahoo.com>
2024-04-16 12:35:20 -0400

    Joining tables in different databases

---
+++ ch05/joining-tables-in-different-databases.sql
@@ -0,0 +1,7 @@
+-- Joining Tables in Different Databases
+select  s.subway_system,
+        s.city,
+        c.country
+from    subway.subway_system as s
+inner join location.country as c
+on      s.country_code = c.country_code;
